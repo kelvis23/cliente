6fc5edce8163cefa1bc3bdb0def8189e67cd8f22
kelvis23 <jesuscuestas116@gmail.com>
2025-10-06 08:54:26 +0200

    5

---
+++ js.arrays/ejercicio.js
@@ -0,0 +1,13 @@
+
+
+   // Se le pide al usuario el número de valores a introducir. Posteriormente se le van pidiendo. Se le devuelven ordenados. ¿Cómo? 😮
+   var valor = Number(prompt("introduce  la cantidad de valores que vas a introducir"));
+
+   for( let i = 0 ; i<valor;i++){
+       let numero = Number(prompt("introduce  los numeros introducir"));
+       numero = valor[i]
+   }
+   // Sistema de inicio de sesión y registro con arrays (menú incluído).
+   // Crea una lista de notas. Calcula la media total y la media sólo de los aprobados. BONUS si se hace con y sin arrays.
+   // Crea un array de 10 valores introducidos por teclado y devuelvelo ordenado (SIN FUNCIONES).
+   // Crea dos arrays de números arr1 y arr2 con 5 elementos cada uno. Recorre ambos arrays y cuenta cuántos elementos son iguales y están en la misma posición. Imprime el resultado.
