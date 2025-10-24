faf323a8a8047fbd571565ae1a1807ff7026470e
kelvis23 <jesuscuestas116@gmail.com>
2025-10-24 09:23:36 +0200

    x

--- js.arrys_metodos/ejercicio.js
+++ js.arrys_metodos/ejercicio.js
@@ -2,7 +2,7 @@
 
 //Creamos un programa que pida números hasta que insertemos un 0, entonces nos indica la longitud del array.
 
-/*
+
 var arr1 = []
 var numero = Number(prompt("introduce un numero"))
 
@@ -14,9 +14,9 @@ while (numero != 0) {
 alert(" la logitud del arry es de " + arr1.length)
 
 
-*/
+
 //Comprobar posteriormente si en el array existe al menos algún 5.
-/*
+
 var arr2 = [21, 5, 34, 6, 10, 7, 1, 5]
 
 if (arr2.includes(5)) {
@@ -24,9 +24,9 @@ if (arr2.includes(5)) {
 } else {
   alert("no se a encontrado nigun 5 en el array")
 }
-*/
+
 //Pedir un número al usuario e indicar cuántas veces aparece en el array anterior.
-/*
+
 var usuario =Number(prompt("introduce un numero"))
 var arr2 = [21, 5, 34, 6, 10, 7, 1, 5]
 count = 0
@@ -42,9 +42,9 @@ if (count >= 1) {
 } else {
   alert("no se a encontrado nigun "+usuario+" en el array")
 }
-  */
+  
 //En un programa con un array predefinido con varios nombres, pedirle uno al usuario y comprobar si está o no.
-/*
+
 var arr3 = ["cesar", "andrea", "kelvis", "jesus", "ana", "pedro", "juan", "maria", "karla"];
 var usuario = prompt("introduce un nombre");
 
@@ -53,7 +53,7 @@ if (arr3.includes(usuario2)) {
 } else {
   alert("en el array no existe el nombre " + usuario)
 }
-*/
+
 //Crea un menú que controle los datos de un array (como una lista de la compra), que permita añadir al final o al principio, eliminar alguno si existe o borrar todos, y a cada paso actualice (o vuelva a mostrar) la lista completa.
 
 
@@ -147,19 +147,8 @@ do {
 
 } while (option !== 6);
 
-
-
-
-
-
-
-
-
-
-
-
 //Dado un array con precios, aplicar a todos un descuento indicado por prompt.
-/*
+
 var descuento = Number(prompt("introduce el descuento"))
 var arr5 = [21, 5, 34, 6, 10, 7, 1, 5]
 let rebaja = descuento * 0.01
@@ -170,9 +159,9 @@ for (const x of arr5) {
 }
 console.log("Precios originales:", arr5);
 console.log("Precios con descuento:", preciosConDescuento);
-*/
+
 //BONUS: Reutiliza el ejercicio 1 y posteriormente devuelve el array ordenado.
-/*
+
 var arr6 = [];
 var numero = Number(prompt("introduce un numero"))
 
@@ -185,4 +174,4 @@ while (numero != 0) {
 for (const x of arr1) {
   console.log(arr1)
 }
-*/
+
