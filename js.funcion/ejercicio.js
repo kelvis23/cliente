4bbea6641568af25daddbfd77d785bde12fb6824
kelvis23 <jesuscuestas116@gmail.com>
2025-11-05 13:03:17 +0100

    hh

--- js.funcion/ejercicio.js
+++ js.funcion/ejercicio.js
@@ -29,7 +29,7 @@ function temperatura(a) {
 console.log(temperatura(100))
 */
 //Función esPrimo() que devuelva booleano.
-
+/*
 function esPrimo(a){
   if(a %2==0){
     return true
@@ -38,14 +38,26 @@ function esPrimo(a){
 }
 console.log(esPrimo(4));
 
-
+*/
 
 
 //Función que cuente el número de vocales de una palabra.
+/*
+function conts(text) {
+  var voc = ["a", "e", "i", "o", "u"]
+  let contador = 0;
 
-function conts (){
-  
+  for (let i = 0; i < text.length; i++) {
+    var caracter = text[i].toLowerCase();
+    if (voc.includes(caracter)) {
+      contador++;
+    }
+  }
+  return contador;
 }
+
+console.log(conts("hola"));
+*/
 //Función que reciba tres parámetros (a,b,c) y devuelva un array con las soluciones de una ecuación de segundo grado. (-b+-sqrt(b**2-4*a*c)/2*a)
 //Crea una función que reciba un número y un callback. Si el número es mayor a 10, ejecuta el callback.
 //Función que sume todos los elementos introducidos como parámetros, sin preestablecer el número de parámetros.
