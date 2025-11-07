322c92c4b4ee286e30196058ad5d434ec1d3c7eb
kelvis23 <jesuscuestas116@gmail.com>
2025-11-07 09:23:34 +0100

    terminado funciones

--- js.funcion/ejercicio.js
+++ js.funcion/ejercicio.js
@@ -48,24 +48,75 @@ function conts(text) {
   let contador = 0;
 
   for (let i = 0; i < text.length; i++) {
+  //toLowerCase : convierte todos los caracteres de una cadena de texto a minúsculas
     var caracter = text[i].toLowerCase();
     if (voc.includes(caracter)) {
       contador++;
     }
-  }
+  }+
   return contador;
 }
 
 console.log(conts("hola"));
 */
 //Función que reciba tres parámetros (a,b,c) y devuelva un array con las soluciones de una ecuación de segundo grado. (-b+-sqrt(b**2-4*a*c)/2*a)
-
+/*
 function ecuasion(a,b,c){
- var s=(-b+-sqrt(b**2-4*a*c)/2*a)
- return s
+  var x =Math.sqrt(b**2-4*a*c)
+ var s1 = (-b-x)/(2*a);
+ var s2 = (-b+x)/(2*a);
+ return [s1,s2]
 }
 
-console.log(ecuasion(4,2,1));
+console.log(ecuasion(1,-5,6));
+
+*/
 //Crea una función que reciba un número y un callback. Si el número es mayor a 10, ejecuta el callback.
+/*
+function mostrar(number) {
+  alert("el numero "+number+" es mayor que 10");
+}
+
+function procesarEntradaUsuario(callback , number) {
+  if (number> 10){
+    callback(number)
+  }
+}
+
+procesarEntradaUsuario(mostrar,11);
+*/
 //Función que sume todos los elementos introducidos como parámetros, sin preestablecer el número de parámetros.
+/*
+function sum(...number) {
+ var sum =0 
+  for (let i = 0; i < number.length; i++) {
+    sum+=number[i]
+  }
+  return sum
+}
+console.log(sum(2,1,5))
+
+*/
 //Función que reciba un array de palabras y devuelva solo las que son palíndromos.
+
+function palíndromos(textos) {
+ var palabras = []
+  for (let i = 0; i < textos.length; i++) {
+    var caracter = textos[i].toLowerCase();
+    // Divide la cadena de texto en un array de caracteres
+    var arrCaracter =caracter.split('')
+    // Invierte el orden de los elementos en el array
+    var invertir = arrCaracter.reverse()
+    //Une los elementos del array invertido en una nueva cadena
+    var palabra = invertir.join('')
+    if( caracter==palabra ){
+      palabras.push(caracter)
+    }
+  }
+    return palabras 
+  
+}
+
+var text=["cama","Ana","Rosa","reconocer","oso"]
+
+console.log(palíndromos(text))
