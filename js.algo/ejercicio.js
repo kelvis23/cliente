e01f143058c022e23c0a28e7c9ac193d2c1b779b
kelvis23 <jesuscuestas116@gmail.com>
2025-11-12 14:04:01 +0100

    xx

---
+++ js.algo/ejercicio.js
@@ -0,0 +1,21 @@
+/*function fibonachi(n){
+  if(n ===0)return 0;
+  if(n ===1)return 1;
+  
+  return fibonachi (n-1)+fibonachi(n-2);
+
+}
+
+console.log (fibonachi(6));
+*/
+
+function sumarArray(arr){
+  if (arr.length ===0){
+    return 0;
+  }
+  return arr[0]+ sumarArray(arr.slice(1))
+}
+
+var arr = [2,4,5, 1,4];
+
+console.log(sumarArray(arr));
