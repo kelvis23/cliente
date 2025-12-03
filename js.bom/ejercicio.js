e884df29aae92d4bd0026d0d202f8fdb7c7d8f17
kelvis23 <jesuscuestas116@gmail.com>
2025-12-03 14:26:29 +0100

    DOM

---
+++ js.bom/ejercicio.js
@@ -0,0 +1,51 @@
+
+/*
+
+alert("continuar ");
+const intervalo = setInterval(()=>{
+   const respuesta = confirm("continuar")
+if(!respuesta){
+    clearInterval(intervalo);
+    console.log("terminado")
+} 
+},5000)
+*/
+
+/*
+let ususario = localStorage.getItem("usuario");
+if (!ususario){
+    ususario = prompt("Como te llamas")
+    if(ususario){
+        localStorage.setItem("usuario",ususario);
+    }else{
+        ususario = "invitado"
+    }
+}
+
+alert( `hola, ${ususario}`)
+
+*/
+/*
+const intervalo = setInterval(()=>{
+   console.log(navigator.onLine? "Conectado" : "Sin conexion");
+
+},5000)
+*/
+
+/*
+console.log(location.href);
+console.log(location.protocol);
+console.log(location.host);
+console.log(location.pathname);
+
+*/
+/*
+setTimeout(() => {
+  location.href = "https://www.google.com";
+}, 5000);
+*/
+
+const mequedo =confirm("deseas moberte ")
+if(mequedo == true){
+      location.href = "http://localhost:3000/js.bom/html1.html"
+}
