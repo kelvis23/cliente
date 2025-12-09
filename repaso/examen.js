12aec39568605a99a4491928f98ab37e259429f8
kelvis23 <jesuscuestas116@gmail.com>
2025-12-09 13:55:58 +0100

    practica_examen

--- repaso/examen.js
+++ repaso/examen.js
@@ -32,10 +32,30 @@ class Libro {
 b1= new Libro ("jarri poter" ,"jk rolin")
 
 
-/*
-3. Función que reciba un arreglo de números y devuelva otro arreglo solo con los números mayores a 10.
 
+//3. Función que reciba un arreglo de números y devuelva otro arreglo solo con los números mayores a 10.
+var array = [4,6,0,13,23,1,6,19]
 
-4. Función que reciba un texto y devuelva cuántas palabras contiene.
-5. Crear clase Tienda que administre productos: agregar, eliminar, cambiar precio, listar.
-*/
+function number(arr1){
+    var result = []
+    for (let n of arr1){
+        if(n >10) result.push(n)
+    }
+return result
+}
+console.log(number(array))
+
+//4. Función que reciba un texto y devuelva cuántas palabras contiene.
+function recipText(text){
+if (typeof text !== "string") return 0;
+
+  // Eliminar espacios extra y dividir por espacios
+  const palabras = text .split(/\s+/);
+
+  // Manejar el caso de texto vacío
+  return text.trim() === "" ? 0 : palabras.length;
+}
+var text =("hola como estas")
+console.log(recipText(text))
+
+//5. Crear clase Tienda que administre productos: agregar, eliminar, cambiar precio, listar.
