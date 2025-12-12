22861e2a076f72160132f6ff7e03ab0349cd7ec1
kelvis23 <jesuscuestas116@gmail.com>
2025-12-12 09:16:33 +0100

    dom2

--- js.dom/ejercicio.js
+++ js.dom/ejercicio.js
@@ -1,4 +1,26 @@
+/*
 setTimeout(() => {
   var nombre = prompt("introduce tu nombre ")
 document.getElementById("cambiar").innerHTML = "Hola "+ nombre;
 }, 1000);
+*/
+
+
+// selecionar a todos los h2 que tengan la clase titulin
+/*
+const titulos = document.querySelectorAll("h2.titulin");
+
+titulos.forEach(titulo => {
+  console.log(titulo.textContent);
+});
+*/
+/* seleciona todos los los p que tengan en un div de clase contenido  */
+const contenidos = document.querySelectorAll("div.contenido p");
+
+contenidos.forEach(contenido =>{
+  console.log(contenido.textContent)
+})
+
+/* todos los elementos con clase archivo y visible(ambas) */
+
+/* los elementos  */
