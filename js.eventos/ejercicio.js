3be99e416600c22dde6fac00fa819c5107533a6b
kelvis23 <jesuscuestas116@gmail.com>
2026-01-12 10:19:37 +0100

    eventos

--- js.eventos/ejercicio.js
+++ js.eventos/ejercicio.js
@@ -1,5 +1,19 @@
 const boton = document.getElementById("miBoton");
-
+/*
 boton.addEventListener("click",function(){
 alert("has echo click")
+});
+*/
+
+
+boton.addEventListener("click",(objEvento)=>{
+console.log(objEvento)
+});
+
+const coordenadas = document.getElementById('coordenadas');
+
+// Escuchar clic en todo el body
+document.body.addEventListener('click', function(evento) {
+   console.log(`(${evento.screenX}),(${evento.screenY})`)
+
 });
